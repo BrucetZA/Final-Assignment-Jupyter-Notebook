bedc03145f515f25a65a99251c5f1c973fdd54e2
BrucetZA <157508121+BrucetZA@users.noreply.github.com>
2024-02-05 10:46:59 +0000

    Working with files in Jupyter Notebooks

---
+++ Final Assignment; Jupyter Notebook.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# __In this notebook, Data Science Tools and Ecosystem are summarized.__
+
+# __Objectives__
+# 
+# * List popular languages for Data Science
+# * List common libraries used for Data Science
+# * Table Data Science tools
+
+# Some of the populare languages that Data Scientists use are:
+
+# 1. Python
+# 2. R.
+# 3. Java
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Numpy
+# 2. Pandas
+# 3. SciPy
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyer Notebook |
+# | RStudio |
+# | Matplotlib |
+
+# ### Below are a few examples of evaluationg arithmetic expressions in Python
+
+# This is a simple arithmetic expression to multiply then add integers
+
+# In[3]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by dividing by 60
+
+# In[4]:
+
+
+200/60
+
+
+# ## Author
+# Bruce Thomson
+
+# In[ ]:
+
+
+
+
